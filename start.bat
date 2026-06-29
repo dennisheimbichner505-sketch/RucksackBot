696386351a0a74c51f025061fd2ce4cb469013ba
dennisheimbichner505 <dennisheimbichner505@gmail.com>
2026-06-29 23:52:42 +0200

    RucksackBot startklar

--- start.bat
+++ start.bat
@@ -1,9 +1,30 @@
 @echo off
+cd /d "%~dp0"
 
-start cmd /k "cd /d %~dp0 && node bot.js"
+echo ====================================
+echo      RucksackBot startet...
+echo ====================================
+echo.
 
+echo [1/4] Suche nach Updates...
+git pull
+
+echo.
+echo [2/4] Installiere Pakete...
+call npm install
+
+echo.
+echo [3/4] Starte Bot...
+start "RucksackBot" cmd /k "cd /d %~dp0 && node bot.js"
+
+echo.
+echo Warte 3 Sekunden...
 timeout /t 3 >nul
 
-start cmd /k "cd /d %~dp0 && ngrok http 3000"
+echo.
+echo [4/4] Starte ngrok...
+start "ngrok" cmd /k "cd /d %~dp0 && ngrok http 3000"
 
-exit
+echo.
+echo Fertig!
+pause
